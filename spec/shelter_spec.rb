d3b8bf5d59d94f4baa5e10dce686bb7a234de405
Anthony Ongaro <43623494+ajongaro@users.noreply.github.com>
2022-10-12 08:46:50 -0700

    Add iteration 4 test

--- spec/shelter_spec.rb
+++ spec/shelter_spec.rb
@@ -79,4 +79,20 @@ RSpec.describe Shelter do
       expect(shelter.over_capacity?).to be true 
     end
   end
+
+  # Iteration 4
+  describe '#adopt' do
+    it 'removes pets until not over capacity' do
+      shelter = Shelter.new('Denver Animal Shelter', 3)
+      
+      shelter.add_pet('Salem')
+      shelter.add_pet('Beethoven')
+      shelter.add_pet('Spot')
+      shelter.add_pet('Jonsey')
+     
+      expect(shelter.over_capacity?).to be true
+      shelter.adopt
+      expect(shelter.over_capacity?).to be false
+    end
+  end
 end
